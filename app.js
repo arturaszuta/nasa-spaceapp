fcfe11603ba81eea9eca4904ed84c512fbadda36
JessieW0010 <wangj.5415@gmail.com>
2019-10-19 12:00:19 -0400

    added insert db query to post user satellite

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
+const { Pool } = require("pg");
 
 app.get("/", function(req, res) {
 	res.send("Hello!");
@@ -17,9 +18,22 @@ app.get("/user/1/satellites", function(req, res) {
 	res.send("pull from db and display satellites belonging to user 1");
 });
 
-app.post("/user/1/satellites", function(req, res) {
-	//change route to /user/:userid/satellites when auth is setup
-	res.send({ message: "success", status: 200 });
+app.post("/user/:user_id/satellites", function(req, res) {
+	const newSatellite = [
+		params.name,
+		params.timestamp,
+		params.description,
+		user_id
+	];
+	return Pool.query(
+		`
+    INSERT INTO satellites (name, created_at, description, user_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;`,
+		newSatellite
+	).then(res => {
+		res.send({ message: "success", status: 200 });
+	});
 });
 
 app.listen(PORT || 8000, () => {
