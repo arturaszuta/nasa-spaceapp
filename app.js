75f3b16658902ab2e37d64478b825b697c775e45
Arturas Zuta <arturaszuta@gmail.com>
2019-10-19 11:22:22 -0400

    basic server and basic dependancy setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+  res.send('Hello!')
+})
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server has started!')
+});
