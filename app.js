e1fe569d7e5fb9e5ffabc64b74c165a896d6d099
JessieW0010 <wangj.5415@gmail.com>
2019-10-19 11:42:40 -0400

    added get routes for user, user satellites, and post route for user satellites

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const app = express()
+// const ENV        = process.env.ENV || "development";
+const PORT = process.env.PORT;
+const express = require("express");
+const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello!')
-})
+app.get("/", function(req, res) {
+	res.send("Hello!");
+});
 
+app.get("/user/1", function(req, res) {
+	//change route to /user/:userid when auth is setup
+	res.send("pull from db and display users here");
+});
 
+app.get("/user/1/satellites", function(req, res) {
+	//change route to /user/:userid/satellites when auth is setup
+	res.send("pull from db and display satellites belonging to user 1");
+});
 
+app.post("/user/1/satellites", function(req, res) {
+	//change route to /user/:userid/satellites when auth is setup
+	res.send({ message: "success", status: 200 });
+});
 
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server has started!')
+app.listen(PORT || 8000, () => {
+	console.log("Server has started!");
 });
