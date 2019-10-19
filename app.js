2f76b0fb52be09ab36fb180fd5a0af72232af922
JessieW0010 <wangj.5415@gmail.com>
2019-10-19 13:50:37 -0400

    added routes for users

--- app.js
+++ app.js
@@ -20,17 +20,30 @@ app.get("/users", function(req, res) {
 	});
 });
 
-app.post("/satellites", function(req, res) {
+app.get("/satellites", function(req, res) {
+	client.query(`SELECT * FROM satellites`, (error, result) => {
+		if (error) {
+			res.json("Something went wrong when fetching your data... ");
+		}
+		res.json(result.rows);
+	});
+});
+
+app.post("/user/1/satellites", function(req, res) {
 	// Only user 1 exists
+	// const user_id = req.params.user_id;
+	const params = req.body;
 	const satelliteParams = [
-		req.params.name,
-		req.params.description,
-		req.params.sat_id
+		params.name,
+		params.description,
+		params.year_launched,
+		params.sat_id,
+		1 /*user_id*/
 	];
 	client
 		.query(
-			`INSERT INTO satellites (name, description, sat_id)
-    VALUES ($1, $2, $3)
+			`INSERT INTO satellites (name, description, year_launched, sat_id, user_id)
+    VALUES ($1, $2, $3, $4, $5)
     RETURNING *`,
 			satelliteParams
 		)
@@ -42,13 +55,21 @@ app.post("/satellites", function(req, res) {
 		});
 });
 
-app.get("/satellites", function(req, res) {
-	client.query(`SELECT * FROM satellites`, (error, result) => {
-		if (error) {
-			res.json("Something went wrong when fetching your data... ");
-		}
-		res.json(result.rows);
-	});
+app.get("user/1/satellites", function(req, res) {
+	// const user_id = req.params.user_id;
+	client
+		.query(
+			`
+        SELECT * FROM user_satellites
+        WHERE user_id = $1`,
+			1 /* user_id */
+		)
+		.then((error, result) => {
+			if (error) {
+				res.json("Something went wrong when fetching your data... ");
+			}
+			res.json(result.rows);
+		});
 });
 
 app.listen(PORT || 8000, () => {
